820969911c47e9bb16db1d24ca5ade495b377e40
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-12-27 02:55:53 -0500

    [client-feature] - signup functionality complete

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LandingService } from './landing.service'
 
 @Component({
   selector: 'app-landing',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  private _signupPassword: string = ''
+  private _signupEmail: string = ''
+  private _signupFirstName: string = ''
+  private _signupLastName: string = ''
+
+  private _loginEmail: string = ''
+  private _loginPassword: string = ''
+
+  constructor(private _landingService: LandingService) { }
+
+  signup() {
+    let firstName = this._signupFirstName
+    let lastName = this._signupLastName
+    let email = this._signupEmail
+    let password = this._signupPassword
+
+    this._landingService.signup(firstName, lastName, email, password)
+      .subscribe(
+        response => {
+          console.log("Response from signup ", response)
+        }
+      )
+  }
 
   ngOnInit() {
+    
   }
 
 }
--- src/app/landing/landing.service.ts
+++ src/app/landing/landing.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable'
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 @Injectable()
 export class LandingService {
 
-  constructor() { }
+  constructor(public http: Http) { }
+
+  signup(firstName, lastName, email, password): Observable<any> {
+    let body = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password
+    }
+    return this.http.post('/api/users/signup', body)
+      .map(Response => {
+        console.log(Response)
+        return Response
+      })
+  }
 
 }
