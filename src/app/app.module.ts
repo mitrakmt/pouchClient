cfd61462b8911c191aaf64e9b94f0239401bd156
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-12-27 02:35:00 -0500

    [client-feature] - login page created, structure completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
+
+import { LandingService } from './landing/landing.service';
+import { NavbarComponent } from './navbar/navbar.component'
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', pathMatch: 'full', component: LandingComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LandingComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    MaterialModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    LandingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
